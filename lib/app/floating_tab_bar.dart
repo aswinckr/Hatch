6e47e90c3cfde59c1bf21c1ec8ab38332d8e7e38
aswinckr <aswinckr@gmail.com>
2026-08-07 16:45:36 +0200

    style: use bowl icon for dishes tab

--- lib/app/floating_tab_bar.dart
+++ lib/app/floating_tab_bar.dart
@@ -82,7 +82,7 @@ class FloatingTabBar extends StatelessWidget {
                         Expanded(
                           child: _Destination(
                             label: 'Dishes',
-                            icon: material.Icons.restaurant_rounded,
+                            icon: material.Icons.soup_kitchen_rounded,
                             selected: selectedIndex == 0,
                             onTap: () => onSelected(0),
                           ),
